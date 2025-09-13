d00616b268001a53c5a3aedaa5064fe978794540
Valentin Negru <nevalenti@proton.me>
2025-09-13 21:12:50 +0300

    disable matchparen

--- lua/options.lua
+++ lua/options.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
-vim.g.loaded_matchparen = 1
 
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
+vim.g.loaded_matchparen = 1
 
 vim.opt.number = true
 vim.opt.relativenumber = true
