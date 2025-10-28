846b5a36182125e2e35f64b0b6a57b8520998b20
Valentin Negru <nevalenti@proton.me>
2025-10-28 12:09:26 +0200

    change <leader>gl to show git log in oneline

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -34,7 +34,7 @@ local map = vim.keymap.set
 local gitsigns = require('gitsigns')
 
 map('n', '<leader>gg',':vertical G<CR>', { desc = "Git status" })
-map('n', '<leader>gl',':vertical G log<CR>', { desc = "Git log" })
+map('n', '<leader>gl',':vertical G log --oneline<CR>', { desc = "Git log" })
 
 map('n', ']c', function()
   if vim.wo.diff then
