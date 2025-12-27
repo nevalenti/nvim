293e1b36e3eab5727e1d2492f684155cb15b3321
Valentin Negru <nevalenti@proton.me>
2025-12-27 15:03:18 +0200

    add dap adapter for js

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,8 +1,39 @@
-require('nvim-dap-virtual-text').setup {}
+local mason_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter"
+
+require('nvim-dap-virtual-text').setup {
+  enabled = true,
+  highlight_changed_variables = true,
+  highlight_new_as_changed = true,
+  show_stop_reason = true,
+  virt_text_pos = 'eol',
+  all_frames = false,
+}
 
 local dap = require('dap')
 local dapui = require('dapui')
-dapui.setup {}
+
+dapui.setup {
+  layouts = {
+    {
+      elements = {
+        { id = "scopes",      size = 0.25 },
+        { id = "breakpoints", size = 0.25 },
+        { id = "stacks",      size = 0.25 },
+        { id = "watches",     size = 0.25 },
+      },
+      size = 40,
+      position = "left",
+    },
+    {
+      elements = {
+        { id = "repl",    size = 0.5 },
+        { id = "console", size = 0.5 },
+      },
+      size = 10,
+      position = "bottom",
+    },
+  },
+}
 
 local map = vim.keymap.set
 
@@ -15,24 +46,24 @@ map('n', '<Leader>B', function()
   dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
 end, { desc = 'Debug: Set Conditional Breakpoint' })
 
-dap.listeners.after.event_initialized['dapui_config'] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated['dapui_config'] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited['dapui_config'] = function()
-  dapui.close()
-end
+map('n', '<Leader>du', dapui.toggle, { desc = 'Debug: Toggle UI' })
+map('n', '<Leader>dt', dap.terminate, { desc = 'Debug: Terminate Session' })
+map('n', '<Leader>de', dapui.eval, { desc = 'Debug: Evaluate Expression (normal/visual mode)' })
+map('v', '<Leader>de', dapui.eval, { desc = 'Debug: Evaluate Selection' })
 
-dap.adapters.netcoredbg = {
+dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+dap.adapters.coreclr = {
   type = 'executable',
   command = 'netcoredbg',
   args = { '--interpreter=vscode' },
 }
+
 dap.configurations.cs = {
   {
-    type = 'netcoredbg',
+    type = 'coreclr',
     name = 'Launch - netcoredbg',
     request = 'launch',
     program = function()
@@ -43,7 +74,7 @@ dap.configurations.cs = {
     console = 'internalConsole',
   },
   {
-    type = 'netcoredbg',
+    type = 'coreclr',
     name = 'Attach - netcoredbg',
     request = 'attach',
     processId = function()
@@ -59,14 +90,64 @@ dap.adapters.python = {
   command = 'python',
   args = { '-m', 'debugpy.adapter' },
 }
+
 dap.configurations.python = {
   {
     type = 'python',
     request = 'launch',
     name = 'Launch file',
     program = '${file}',
-    pythonPath = function()
-      return 'python'
-    end,
+    pythonPath = 'python',
+    console = 'integratedTerminal',
   },
 }
+
+dap.adapters['pwa-node'] = {
+  type = 'server',
+  host = 'localhost',
+  port = '${port}',
+  executable = {
+    command = 'node',
+    args = {
+      mason_path .. '/js-debug/src/dapDebugServer.js',
+      '${port}'
+    },
+  },
+}
+
+dap.adapters['pwa-chrome'] = dap.adapters['pwa-node']
+
+local js_ts_configs = {
+  {
+    type = 'pwa-node',
+    request = 'launch',
+    name = 'Launch current file (Node.js)',
+    program = '${file}',
+    cwd = '${workspaceFolder}',
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+    skipFiles = { '<node_internals>/**', 'node_modules/**' },
+  },
+  {
+    type = 'pwa-node',
+    request = 'attach',
+    name = 'Attach to Node.js process',
+    processId = require('dap.utils').pick_process,
+    cwd = '${workspaceFolder}',
+    sourceMaps = true,
+    skipFiles = { '<node_internals>/**', 'node_modules/**' },
+  },
+  {
+    type = 'pwa-chrome',
+    request = 'launch',
+    name = 'Launch Chrome<a href="http://localhost:3000" target="_blank" rel="noopener noreferrer nofollow"></a>',
+    url = 'http://localhost:3000',
+    webRoot = '${workspaceFolder}',
+    sourceMaps = true,
+  },
+}
+
+for _, lang in ipairs({ 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' }) do
+  dap.configurations[lang] = js_ts_configs
+end
