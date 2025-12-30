497aecf526940cef5098ae594e4b012ae7fbbf46
Valentin Negru <nevalenti@proton.me>
2025-12-30 13:32:07 +0200

    correct import order

--- init.lua
+++ init.lua
@@ -1,10 +1,25 @@
 vim.pack.add({
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/nvim-neotest/nvim-nio" },
+  { src = "https://github.com/echasnovski/mini.icons" },
+
+  { src = "https://github.com/nevalenti/gruber-darker.nvim" },
+  { src = "https://github.com/nvim-lualine/lualine.nvim" },
+  { src = "https://github.com/stevearc/dressing.nvim" },
+  { src = "https://github.com/j-hui/fidget.nvim" },
+  { src = "https://github.com/folke/which-key.nvim" },
+
+  { src = "https://github.com/nvim-telescope/telescope.nvim" },
+
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/refractalize/oil-git-status.nvim" },
+
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 
+  { src = "https://github.com/VonHeikemen/lsp-zero.nvim" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-  { src = "https://github.com/VonHeikemen/lsp-zero.nvim" },
   { src = "https://github.com/hrsh7th/nvim-cmp" },
   { src = "https://github.com/hrsh7th/cmp-buffer" },
   { src = "https://github.com/hrsh7th/cmp-path" },
@@ -14,56 +29,54 @@ vim.pack.add({
   { src = "https://github.com/saadparwaiz1/cmp_luasnip" },
   { src = "https://github.com/rafamadriz/friendly-snippets" },
 
-  { src = "https://github.com/mfussenegger/nvim-dap" },
-  { src = "https://github.com/jay-babu/mason-nvim-dap.nvim" },
-  { src = "https://github.com/rcarriga/nvim-dap-ui" },
-  { src = "https://github.com/theHamsta/nvim-dap-virtual-text" },
-  { src = "https://github.com/nvim-neotest/nvim-nio" },
-  { src = "https://github.com/leoluz/nvim-dap-go" },
-  { src = "https://github.com/mfussenegger/nvim-dap-python" },
-
-  { src = "https://github.com/nvim-telescope/telescope.nvim" },
-  { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/stevearc/oil.nvim" },
-  { src = "https://github.com/refractalize/oil-git-status.nvim" },
+  { src = "https://github.com/folke/trouble.nvim" },
 
   { src = "https://github.com/tpope/vim-fugitive" },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
   { src = "https://github.com/sindrets/diffview.nvim" },
 
+  { src = "https://github.com/mbbill/undotree" },
+
+  { src = "https://github.com/mfussenegger/nvim-dap" },
+  { src = "https://github.com/jay-babu/mason-nvim-dap.nvim" },
+  { src = "https://github.com/rcarriga/nvim-dap-ui" },
+  { src = "https://github.com/theHamsta/nvim-dap-virtual-text" },
+  { src = "https://github.com/leoluz/nvim-dap-go" },
+  { src = "https://github.com/mfussenegger/nvim-dap-python" },
+
   { src = "https://github.com/tpope/vim-commentary" },
   { src = "https://github.com/tpope/vim-surround" },
   { src = "https://github.com/m4xshen/autoclose.nvim" },
-  { src = "https://github.com/mbbill/undotree" },
-
-  { src = "https://github.com/folke/trouble.nvim" },
-  { src = "https://github.com/nvim-lualine/lualine.nvim" },
-  { src = "https://github.com/folke/which-key.nvim" },
-  { src = "https://github.com/echasnovski/mini.icons" },
-
-  { src = "https://github.com/nevalenti/gruber-darker.nvim" },
 
   { src = "https://github.com/olimorris/codecompanion.nvim" },
   { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },
-  { src = "https://github.com/stevearc/dressing.nvim" },
-  { src = "https://github.com/j-hui/fidget.nvim" }
 })
 
 require('options')
 require('mappings')
 require('autocommands')
-require('overrides')
-require('theme')
 
-require('plugins.lsp')
-require('plugins.dap')
-require('plugins.telescope')
-require('plugins.oil')
-require('plugins.gitsigns')
-require('plugins.trouble')
-require('plugins.undotree')
-require('plugins.autoclose')
+require('theme')
 require('plugins.mini-icons')
 require('plugins.statusline')
-require('plugins.render-markdown')
+
+require('plugins.telescope')
+
+require('plugins.oil')
+
+require('plugins.lsp')
+
+require('plugins.trouble')
+
+require('plugins.gitsigns')
+
+require('plugins.undotree')
+
+require('plugins.dap')
+
+require('plugins.autoclose')
+
 require('plugins.codecompanion')
+require('plugins.render-markdown')
+
+require('overrides')
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,30 @@
 local lsp = require('lsp-zero')
-local cmp = require('cmp')
 
-local map = vim.keymap.set
+lsp.extend_lspconfig({
+  sign_icons = true,
+  lsp_attach = function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.jump_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.jump_prev() end, opts)
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  end,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
 
 require("mason").setup {}
 require('mason-lspconfig').setup({
   ensure_installed = {
-    'csharp_ls',
-    'gopls',
-    'pyright',
-    'ruff',
-    'ts_ls',
-    'lua_ls',
-    'html',
-    'cssls',
-    'tailwindcss',
-    'jsonls',
-    'lemminx',
-    'yamlls',
-    'taplo',
+    'csharp_ls', 'gopls', 'pyright', 'ruff', 'ts_ls',
+    'lua_ls', 'html', 'cssls', 'tailwindcss',
+    'jsonls', 'lemminx', 'yamlls', 'taplo',
   },
   handlers = {
     function(server_name)
@@ -30,65 +36,37 @@ require('mason-lspconfig').setup({
     end,
   },
 })
-require('nvim-treesitter.configs').setup {
-  ensure_installed = {
-    'c_sharp',
-    'python',
-    'go',
-    'gomod',
-    'gowork',
-    'gosum',
-    'javascript',
-    'typescript',
-    'lua',
-    'html',
-    'css',
-    'json',
-    'xml',
-    'yaml',
-    'toml',
-  },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
 
-lsp.on_attach(function(_, bufnr)
-  local clients = vim.lsp.get_clients({ bufnr = bufnr })
-
-  local lsp_names = {}
-  for _, client in ipairs(clients) do
-    table.insert(lsp_names, client.name)
-  end
-
-  local opts = { buffer = bufnr, remap = false }
-
-  map("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  map("n", "K", function() vim.lsp.buf.hover() end, opts)
-  map("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  map("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
-  map("n", "[d", function() vim.diagnostic.jump_next() end, opts)
-  map("n", "]d", function() vim.diagnostic.jump_prev() end, opts)
-  map("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-  map("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-  map("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-  map("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
-
+local cmp = require('cmp')
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
   sources = {
     { name = 'nvim_lsp' },
     { name = 'buffer' },
     { name = 'path' },
     { name = 'luasnip' },
   },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
 })
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    'c_sharp', 'python', 'go', 'gomod', 'gowork', 'gosum',
+    'javascript', 'typescript', 'lua', 'html', 'css',
+    'json', 'xml', 'yaml', 'toml',
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  auto_install = true,
+}
